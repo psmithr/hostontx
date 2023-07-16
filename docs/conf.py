b819ee273620f08feb91a7005d96be58a84c3a6d
psmithr <139660756+psmithr@users.noreply.github.com>
2023-07-16 10:41:31 +0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Tutorial Suara Google'
-copyright = u'2022, Karinov Team'
+project = u'US Wiki'
+copyright = u'2023, KNOT35'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
